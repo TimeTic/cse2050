029db65422562920d49e4a615d1f8d72e18f3ff3
TimeTic <zahidullah.halimi14@gmail.com>
2024-01-18 11:13:07 -0800

    HW files

---
+++ HW1/fizzbuzz.py
@@ -0,0 +1,17 @@
+###############################################################################
+# Collaboration                                                               #
+# -------------                                                               #
+# You can collaborate with up to 3 classmates (for a total of 4 students per  #
+# group). If you do so, remember not to share code directly. Discussions are  #
+# fine, code sharing is not. Also note that all have to submit individually.  #
+#                                                                             #
+# Enter any collaborators here:                                               #
+# Collaborator 1:                                                             #
+# Collaborator 2:                                                             #
+# Collaborator 3:                                                             #
+###############################################################################
+
+def fizzbuzz(start, finish):
+    pass
+    # Delete this comment and below, then start working.
+    # Make sure you add a docstring to this function.
