8dcf9581e877eabcfee09ba0568122f046da8ed9
TimeTic <zahidullah.halimi14@gmail.com>
2024-01-18 14:20:08 -0800

    Finished fizzbuzz.py

--- HW1/fizzbuzz.py
+++ HW1/fizzbuzz.py
@@ -11,7 +11,36 @@
 # Collaborator 3:                                                             #
 ###############################################################################
 
+
+# def fizzbuzz(start, finish):
+#     """Fizz, Buzz, FizzBuzz"""
+#     for i in range(start, finish):
+#         if i % 3 == 0 and i % 5 == 0:
+#             print("FizzBuzz")
+#         elif i % 3 == 0:
+#             print("Fizz")
+#         elif i % 5 == 0:
+#             print("Buzz")
+#         else: 
+#             print(i)
+# fizzbuzz(1,101)
+
+#-----------------------------------------------------------------------#
+
 def fizzbuzz(start, finish):
-    pass
-    # Delete this comment and below, then start working.
-    # Make sure you add a docstring to this function.
+    """Fizz, Buzz, FizzBuzz"""
+    for i in range(start, finish):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0 or '3' in str(i) or i % 5 == 0 or '5' in str(i):
+            output = ""
+            if i % 3 == 0 or '3' in str(i):
+                output += "Fizz"
+            if i % 5 == 0 or '5' in str(i):
+                output += "Buzz"
+            print(output)
+        else:
+            print(i)
+fizzbuzz(1, 101)
+
+
