62171e9ec023dc61e855d1aa24b4fc497c7325f1
TimeTic <zahidullah.halimi14@gmail.com>
2024-02-27 19:50:37 -0500

    update

--- HW5/ways_to_sum.py
+++ HW5/ways_to_sum.py
@@ -1,6 +1,30 @@
 
-def ways_to_sum_memo(n, total):
-    pass
+def ways_to_sum_memo(n, total, memo={}):
+    if total == 0:
+        return 1
+    if total < 0:
+        return 0
+    if (n, total) in memo:
+        return memo[(n, total)]
+
+    count = 0
+    for i in range(1, n+1):
+        count += ways_to_sum_memo(n, total - i, memo)
+    memo[(n, total)] = count
+    return count
+
 
 def ways_to_sum_tab(n, total):
-    pass
+    die_table = [0 for _ in range(total + 1)]
+    die_table[0] = 1
+
+    for i in range(1, n+1):
+        for j in range(i, total+1):
+            # if j >= i:
+                # die_table[j] += die_table[j-i]  #problem here, idk why its not outputing 4 and 4 
+                die_table[i] += die_table[j-1]
+    return die_table[total]
+
+
+print(ways_to_sum_memo(n=4, total=3))
+print(ways_to_sum_tab(n=4, total=3))
